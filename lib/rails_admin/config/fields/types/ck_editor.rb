2287d39cbc781c31a0dfbc57a00a7c38010d343a
Bart Elison <bart.elison@sleepygiant.com>
2013-10-08 16:17:37 -0600

    trying a different config route

--- lib/rails_admin/config/fields/types/ck_editor.rb
+++ lib/rails_admin/config/fields/types/ck_editor.rb
@@ -21,7 +21,7 @@ module RailsAdmin
 
           #Use this if you want to point to a cloud instances of the base CKeditor
           register_instance_option :base_location do
-            "ckeditor/"
+            "#{Rails.application.config.assets.prefix}/ckeditor/"
           end
 
           register_instance_option :partial do
